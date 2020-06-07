0390daf80495c8222d2ec46e0a5bcce94f7ac95e
tomas2211 <tomas2211@post.cz>
2020-06-07 19:31:22 +0200

    Testing comm to Display

---
+++ dt-tx/dt_tx.ino
@@ -0,0 +1,101 @@
+#include "Arduino.h"
+
+#include <SPI.h>
+#include "RFM69.h"
+#include "RFM69_ATC.h"
+
+// RFM69 defines
+#define NODEID        0   // keep UNIQUE for each node on same network
+#define NETWORKID     100  // keep IDENTICAL on all nodes that talk to each other
+#define RECVID     1    // "central" node
+#define FREQUENCY   RF69_433MHZ
+#define ENCRYPTKEY    "sampleEncryptKey"
+
+
+RFM69_ATC radio(10, 2, false);
+byte ackCount = 0;
+uint32_t packetCount = 0;
+
+
+uint8_t buff[RF69_MAX_DATA_LEN];
+int buff_idx = 0;
+
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    Serial.println("START - TX module");
+
+    Serial.println("INIT radio");
+    radio.initialize(FREQUENCY, NODEID, NETWORKID);
+    radio.encrypt(ENCRYPTKEY);
+
+    Serial.println("RFM69_ATC Enabled (Auto Transmission Control)");
+
+}
+
+void sendBuffer() {
+    if (buff_idx > 0) {
+        Serial.print("Sending[");
+        Serial.print(buff_idx);
+        Serial.print("]: ");
+        for (byte i = 0; i < buff_idx; i++)
+            Serial.print((char) buff[i]);
+
+        if (radio.sendWithRetry(RECVID, buff, buff_idx))
+            Serial.println(" ok!");
+        else Serial.println(" nothing...");
+    }
+    buff_idx = 0;
+}
+
+void loop() {
+    //process any serial input
+    while (Serial.available() > 0) {
+        char input = Serial.read();
+        if (input == '\n') {
+            sendBuffer();
+        } else {
+            buff[buff_idx] = input;
+            buff_idx++;
+            if (buff_idx >= RF69_MAX_DATA_LEN) {
+                sendBuffer();
+            }
+        }
+    }
+
+    if (radio.receiveDone()) {
+        Serial.print("#[");
+        Serial.print(++packetCount);
+        Serial.print(']');
+        Serial.print('[');
+        Serial.print(radio.SENDERID, DEC);
+        Serial.print("] ");
+
+        for (byte i = 0; i < radio.DATALEN; i++)
+            Serial.print((char) radio.DATA[i]);
+        Serial.print("   [RX_RSSI:");
+        Serial.print(radio.RSSI);
+        Serial.print("]");
+
+        if (radio.ACKRequested()) {
+            byte theNodeID = radio.SENDERID;
+            radio.sendACK();
+            Serial.print(" - ACK sent.");
+
+            // When a node requests an ACK, respond to the ACK
+            // and also send a packet requesting an ACK (every 3rd one only)
+            // This way both TX/RX NODE functions are tested on 1 end at the GATEWAY
+            if (ackCount++ % 3 == 0) {
+                Serial.print(" Pinging node ");
+                Serial.print(theNodeID);
+                Serial.print(" - ACK...");
+                delay(3); //need this when sending right after reception .. ?
+                if (radio.sendWithRetry(theNodeID, "ACK TEST", 8, 0))  // 0 = only 1 attempt, no retries
+                    Serial.print("ok!");
+                else Serial.print("nothing");
+            }
+        }
+        Serial.println();
+    }
+
+}
--- weather_symbols/export_to_bin.py
+++ weather_symbols/export_to_bin.py
@@ -5,86 +5,104 @@ import cv2
 import cairosvg
 from PIL import Image
 import io
+import glob
+import os
 
 
-img = Image.open(
-    io.BytesIO(cairosvg.svg2png(url='175855-weather-icon-compilation/svg/mist.svg',
-                                output_width=74, output_height=74))
-)
+def load_svg(fn, h, w):
+    img = Image.open(
+        io.BytesIO(cairosvg.svg2png(url=fn, output_width=w, output_height=h))
+    )
+    img_np = np.asanyarray(img).astype(np.float32) / 255.0
+    return img_np
 
 
-img_np = np.asanyarray(img).astype(np.float32) / 255.0
-# img = imageio.imread('175855-weather-icon-compilation/png/drizzle.png').astype(np.float32) / 255.0
-# img = cv2.resize(img, dsize=(25, 25), interpolation=cv2.INTER_CUBIC)
+def encode_img(img_bin, var_name, bits=4, debug=False):
+    assert bits in [2, 4, 8]
 
-img_np = img_np[:,:,3] #np.sum(img_np[:,:,3],axis=2) / 3.0
+    img_lin = img_bin.reshape(-1).astype(np.bool)
+    # zrs = np.zeros(-img_lin.shape[0] % 8, dtype=np.bool) # ceil the len to multiple of 8
+    # img_lin = np.hstack((img_lin, zrs))
+    #
+    # img_p = img_lin.reshape(-1,8).astype(np.uint8)
+    #
+    # for i in range(8):
+    #     img_p[:, i] <<= 7 - i
+    #
+    # img_bytes = np.sum(img_p, axis=1)
 
-img_bin = img_np > 0.5
+    out = []
+    ind = 0
+    act_col = False
+    while ind < img_lin.shape[0]:
+        sel = img_lin[ind: (ind + bits ** 2)]
 
-plt.imshow(img_bin)
-# plt.colorbar()
-plt.show()
-
-img_lin = img_bin.reshape(-1).astype(np.bool)
-# zrs = np.zeros(-img_lin.shape[0] % 8, dtype=np.bool) # ceil the len to multiple of 8
-# img_lin = np.hstack((img_lin, zrs))
-#
-# img_p = img_lin.reshape(-1,8).astype(np.uint8)
-#
-# for i in range(8):
-#     img_p[:, i] <<= 7 - i
-#
-# img_bytes = np.sum(img_p, axis=1)
-
-
-bits = 4
-
-out = []
-ind = 0
-act_col = False
-while ind < img_lin.shape[0]:
-    sel = img_lin[ind: (ind + bits ** 2)]
-
-    changes = np.nonzero(sel != act_col)[0]
-    if len(changes) == 0:
-        out.append(bits ** 2 - 1)
-        ind += bits ** 2 - 1
-    else:
-        chg = changes[0]
-        if chg == 0:
-            out.append(0)
-            ind += 1
-            act_col = not act_col
+        changes = np.nonzero(sel != act_col)[0]
+        if len(changes) == 0:
+            out.append(bits ** 2 - 1)
+            ind += bits ** 2 - 1
         else:
-            out.append(chg)
-            ind += chg
+            chg = changes[0]
+            if chg == 0:
+                out.append(0)
+                ind += 1
+                act_col = not act_col
+            else:
+                out.append(chg)
+                ind += chg
 
-print('Prev size (B):', len(img_lin) / 8)
-print('Compressed to (B): ',(len(out) * bits) / 8)
+    if debug:
+        print('Prev size (B):', len(img_lin) / 8)
+        print('Compressed to (B): ', (len(out) * bits) / 8)
 
-assert bits in [2,4,8]
+    coded = []
+    pushed = 0
+    cur = 0
+    for nib in out:
+        cur |= nib
+        pushed += bits
 
-coded = []
-pushed = 0
-cur = 0
-for nib in out:
-    cur |= nib
-    pushed += bits
+        if pushed == 8:
+            coded.append(cur)
+            cur = 0
+            pushed = 0
 
-    if pushed == 8:
+        cur <<= bits
+
+    if pushed != 0:
         coded.append(cur)
-        cur = 0
-        pushed = 0
 
-    cur <<= bits
+    str_out = 'const uint8_t %s[] PROGMEM = {\n\t' % var_name
+    for c in coded:
+        str_out += '%#04x, ' % c
 
-if pushed != 0:
-    coded.append(cur)
+    str_out = str_out[:-2] + '\n};'
+    print(str_out)
 
-str_out = 'const uint8_t sun[] PROGMEM = {\n\t'
-for c in coded:
-    str_out += '%#04x, ' % c
 
-str_out = str_out[:-2] + '\n};'
+PATH_IN = 'flaticon_icons/icons/svg/*.svg'
+H,W = (50,50)
+fn_list = glob.glob(PATH_IN)
 
-print(str_out)
+for fn in fn_list:
+    # Generate binary images
+    img = load_svg(fn, H, W)
+    # img_bw = np.sum(img[:, :, :3], axis=2) / 3
+    img_bin = (img[:, :, 3] < 0.5).astype(np.uint8) * 255
+
+    folder_in, basename = os.path.split(fn)
+    folder_root, _ = os.path.split(folder_in)
+
+    folder_out = os.path.join(folder_root, 'binary')
+    if not os.path.exists(folder_out):
+        os.mkdir(folder_out)
+
+    basename_out = basename[:-3] + 'png'
+
+    imageio.imwrite(os.path.join(folder_out, basename_out), img_bin)
+
+
+
+
+plt.imshow(img_bw)
+plt.show()
